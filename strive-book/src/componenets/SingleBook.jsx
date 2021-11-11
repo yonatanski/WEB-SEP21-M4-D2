4b83c735638b4409d70de31f7c75411b09e875c7
yonatanski <phpcodeyg@gmail.com>
2021-11-11 15:58:53 +0100

    done
    day 3 pair programing done

--- strive-book/src/componenets/SingleBook.jsx
+++ strive-book/src/componenets/SingleBook.jsx
@@ -1,18 +1,28 @@
+import React from "react"
 import {Card,Button,Container,Col,Row} from "react-bootstrap"
 
 
-const SingleBook =({book}) => (
+class SingleBook extends React.Component   {
 
+   state ={
+       selected: false
+   }
+
+
+    render(){
+        return (
     <Container>
     
     <Row>
       <Col className="d-flex flex-wrap book-card">
       <Card 
             className="mt-2 mb-2 ml-3"
+            style ={{border: this.state.selected? "3px solid green" :"none"}}
+            onClick={()=> this.setState({selected:!this.state.selected})}
          >
-            <Card.Img variant="top" src={book.img} className="img-fluid book-card" />
+            <Card.Img variant="top" src={this.props.book.img} className="img-fluid book-card" />
             <Card.Body> 
-              <Card.Title className="mx-n3 my-n2">{book.title}</Card.Title>
+              <Card.Title className="mx-n3 my-n2">{this.props.book.title}</Card.Title>
               <Card.Text>
                
               </Card.Text>
@@ -22,5 +32,10 @@ const SingleBook =({book}) => (
       </Col>
     </Row>
   </Container>
-)
+  )
+  }
+}
+
+   
+
 export default SingleBook;
